b477f929ead1d101054da7f2d0027646a5356e3e
h <dangerousElectrician@gmail.com>
2015-06-06 18:12:39 -0500

    added some comments

--- MPU6050_Spherical.ino
+++ MPU6050_Spherical.ino
@@ -1,14 +1,13 @@
 
-
+//need to include all this if not already included
 #include "I2Cdev.h"
 #include "MPU6050.h"
-
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     #include "Wire.h"
 #endif
-#include "GravVector.h"
+#include "GravVector.h" //stuff for getting acceleration vector
 
-GravVector gv;
+GravVector gv;   //create object to get acceleration vector
 
 void setup() {  
 
@@ -18,11 +17,14 @@ void setup() {
 
 void loop() {
   
-    gv.update();
+    gv.update(); //need to run this to read sensor and calculate spherical coordinates
     
     
-    Serial.print(gv.getRho()); Serial.print(",\t");
-    Serial.print(gv.getTh()); Serial.print(",\t");
-    Serial.print(gv.getPhi()); Serial.println(",\t");
+    //getting the spherical coordinates
+    Serial.print(gv.getRho()); Serial.print(",\t");     //this would be magnitude
+    
+    Serial.print(gv.getTh()); Serial.print(",\t");      //this could be angle of descent
+    Serial.print(gv.getPhi()); Serial.println(",\t");   //or this could be angle of descent
+                                                        //depends on how the sensor is mounted
 
 }
